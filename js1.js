e486dd5d818face721e62ef3a95d8996ff1047ff
Amit Ranjan <98614510+amyie567@users.noreply.github.com>
2024-06-10 22:13:35 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,21 @@
+// exercise-1
+console.log(Boolean(""));
+// output = false
+console.log(Boolean([]));
+//output= true
+console.log(Boolean({}));
+// output =true
+console.log(Boolean(0));
+// output = false
+
+// EXERCISE -2
+console.log(Number(" "));
+//output =0
+console.log(parseInt(" "));
+// output =NaN
+
+// case 2
+console.log(Number("7*3"));
+// NaN(not a Number)
+console.log(parseInt("7*3"));
+// 7
